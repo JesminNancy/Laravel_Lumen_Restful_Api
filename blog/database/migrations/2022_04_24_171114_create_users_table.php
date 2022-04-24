f7e302fc128f1cd13b07a096d387726cf8de4c1d
JesminNancy <jesminnancy@hotmail.com>
2022-04-24 23:20:43 +0600

    17th Commits

---
+++ blog/database/migrations/2022_04_24_171114_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->String('name', 50);
+            $table->String('email', 50);
+            $table->String('password', 50);
+            $table->String('city', 50);
+            $table->String('phone', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
